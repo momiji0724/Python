927a95dc3ba3db19b8e1ea5cf9cf4d2725593aea
NOZOMIのPC\nozok <momiji0724@users.noreply.github.com>
2026-03-20 23:57:37 +0900

    20260320の自己学習

---
+++ SampleCode/samples/selfpy/chap02/practice/practice2_2.py
@@ -0,0 +1,20 @@
+# -*- coding: shift_jis -*-
+#16進数リテラル
+print(0x123)
+
+#数値セパレータ
+num = 123_456_678
+print(num)
+
+#エスケープシーケンスによる改行を含んだ文字列
+print("こんにちは"\
+        "世界")
+
+#指数表現
+num2 = 13456435e7
+num3 = 13193570e-7
+print(num2)
+print(num3)
+
+#raw文字列
+print(r'C:\Users\nozok\OneDrive\画像\作成用\一時保管')
