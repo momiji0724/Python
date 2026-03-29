201e86b9fb450dbb85a77f86ea3d22756ef0433a
NOZOMIのPC\nozok <momiji0724@users.noreply.github.com>
2026-03-29 23:56:01 +0900

    20260329

--- SampleCode/samples/selfpy/chap04/practice/pra01.py
+++ SampleCode/samples/selfpy/chap04/practice/pra01.py
@@ -1,13 +1,13 @@
 # -*- coding: shift_jis -*-
 point = 75;
-if(point < 50):
-    print("不可");
-elif(point < 70):
-    print("可");
-elif(point < 90):
-    print("良");
+if point < 50:
+    print("不可")
+elif point < 70:
+    print("可")
+elif point < 90:
+    print("良")
 else:
-    print("優");
+    print("優")
 
 
 
